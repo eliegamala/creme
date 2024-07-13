567c215565226e00580454c6fb620a64ce4ee63b
eliegamala <eliegamala6@gmail.com>
2024-07-13 21:23:45 +0200

    first commit

---
+++ javascript/data1.js
@@ -0,0 +1,316 @@
+//save the data
+
+const blogArticles = [
+  {
+    image: 'images/popularitem1.jpg',
+    title: 'delicious',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/article1.html'
+
+  },
+  {
+    image: 'images/popularitem2.jpg',
+    title: 'orange cake',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/vanillaCake.html'
+
+  },
+
+  {
+    image: 'images/popularitem4.jpg',
+    title: 'chocalate icecream',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/chocolateicecrem.html'
+
+  },
+  
+];
+
+
+let articlesHTML = "";
+
+blogArticles.forEach((articles) =>{
+
+  articlesHTML += `
+  <div class="popular-items">
+          <div class="popular-image">
+            <img src="${articles.image}" alt="">
+          </div>
+
+          <div class="item-info">
+
+            <h2 class="title">${articles.title}</h2>
+
+            <h2 class="description">${articles.description}</h2>
+  
+            <a href="${articles.articleLink}" target="_blank">Read more  <i class='bx bx-right-arrow-alt'></i></a>
+
+          </div>
+        </div>
+  `
+
+});
+
+document.querySelector('.js-popular-items-container').innerHTML = articlesHTML;
+
+
+const latestBlogArticles = [
+  {
+    image: 'images/popularitem1.jpg',
+    title: 'Delicious',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/article1.html'
+  },
+  {
+    image: 'images/popularitem2.jpg',
+    title: 'Orange Cake',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/vanillaCake.html'
+  },
+  {
+    image: 'images/popularitem4.jpg',
+    title: 'Chocolate Ice Cream',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/chocolateicecrem.html'
+  }
+];
+
+let latestblogHTML = "";
+
+blogArticles.forEach((articles) =>{
+
+  latestblogHTML += `
+  <div class="popular-items">
+          <div class="popular-image">
+            <img src="${articles.image}" alt="">
+          </div>
+
+          <div class="item-info">
+
+            <h2 class="title">${articles.title}</h2>
+
+            <h2 class="description">${articles.description}</h2>
+  
+            <a href="${articles.articleLink}" target="_blank">Read more  <i class='bx bx-right-arrow-alt'></i></a>
+
+          </div>
+        </div>
+  `
+
+});
+
+document.querySelector('.latest-blog .js-popular-items-container').innerHTML = latestblogHTML;
+
+
+//special menu p
+const specialProducts = [{
+  image: 'images/specialfood1.jpg',
+  itemName: 'cherry cake',
+  itemDescription: 'lorem ipsun dokar sit',
+  priceCents: 43,
+},
+
+{
+  image: 'images/specialfood2.jpg',
+  itemName: 'strawberry cake',
+  itemDescription: 'heaven on eart ipsum cherry',
+  priceCents: 54,
+},
+{
+  image: 'images/speicalfood3.jpg',
+  itemName: 'vanila cake',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 100,
+},
+{
+  image: 'images/specialfood4.jpg',
+  itemName: 'strawberry cupcuke',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 32,
+}
+
+];
+
+
+//breakfast menu
+const breakfastProducts = [{
+  image: 'images/breakfast1.jpg',
+  itemName: 'croissant and cafe',
+  itemDescription: 'lorem ipsun dokar sit',
+  priceCents: 43,
+},
+
+{
+  image: 'images/breakfast2.jpg',
+  itemName: '2 coffees special',
+  itemDescription: 'heaven on eart ipsum cherry',
+  priceCents: 54,
+},
+{
+  image: 'images/breakfast3.jpg',
+  itemName: 'cookie and coffee',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 100,
+},
+{
+  image: 'images/breakfast4.jpg',
+  itemName: 'slice cake and coffee',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 32,
+}
+
+];
+
+//dinner menu
+const dinnerProducts = [{
+  image: 'images/dinner1.jpg',
+  itemName: 'lemon cake',
+  itemDescription: 'lorem ipsun dokar sit',
+  priceCents: 43,
+},
+
+{
+  image: 'images/dinner2.jpg',
+  itemName: 'circle cake',
+  itemDescription: 'heaven on eart ipsum cherry',
+  priceCents: 54,
+},
+{
+  image: 'images/dinner3.jpg',
+  itemName: 'strawberry pie cake',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 100,
+},
+{
+  image: 'images/dinner4.jpg',
+  itemName: 'croissant',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 32,
+}
+
+];
+
+//lunch menu
+
+const lunchProducts = [{
+  image: 'images/lunch1.jpg',
+  itemName: 'waffle and cream',
+  itemDescription: 'lorem ipsun dokar sit',
+  priceCents: 43,
+},
+
+{
+  image: 'images/lunch2.jpg',
+  itemName: 'waffle and fruit',
+  itemDescription: 'heaven on eart ipsum cherry',
+  priceCents: 54,
+},
+{
+  image: 'images/lunch3.jpg',
+  itemName: 'pancake with strawberry',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 100,
+},
+{
+  image: 'images/lunch4.jpg',
+  itemName: 'bread and berries',
+  itemDescription: 'lorem ipsun dokar sit ipsimslo',
+  priceCents: 32,
+}
+
+];
+
+
+
+// JavaScript
+function showMenu(category) {
+  const menuItemsContainer = document.getElementById('menu-items-container');
+  let menuItemsHTML = '';
+
+  switch (category) {
+    case 'special':
+      specialProducts.forEach((special) => {
+        menuItemsHTML += `
+          <div class="menu-items">
+            <div class="menu-image">
+              <img src="${special.image}">
+            </div>
+            <div class="menu-info">
+              <h2>${special.itemName}</h2>
+              <p>${special.itemDescription}</p>
+              <span>R:${special.priceCents}</span>
+            </div>
+          </div>`;
+      });
+      break;
+
+    case 'breakfast':
+      breakfastProducts.forEach((breakfast)=>{
+        menuItemsHTML += `
+          <div class="menu-items">
+            <div class="menu-image">
+              <img src="${breakfast.image}">
+            </div>
+            <div class="menu-info">
+              <h2>${breakfast.itemName}</h2>
+              <p>${breakfast.itemDescription}</p>
+              <span>R:${breakfast.priceCents}</span>
+            </div>
+          </div>`;
+
+      });
+      break;
+
+    case 'lunch':
+    lunchProducts.forEach((lunch)=>{
+      menuItemsHTML += `
+      <div class="menu-items">
+          <div class="menu-image">
+            <img src="${lunch.image}">
+          </div>
+          <div class="menu-info">
+            <h2>${lunch.itemName}</h2>
+            <p>${lunch.itemDescription}</p>
+            <span>R:${lunch.priceCents}</span>
+          </div>
+        </div>`
+      
+    });
+      break;
+
+    case 'dinner':
+      dinnerProducts.forEach((dinner)=>{
+        menuItemsHTML += `
+        <div class="menu-items">
+            <div class="menu-image">
+              <img src="${dinner.image}">
+            </div>
+            <div class="menu-info">
+              <h2>${dinner.itemName}</h2>
+              <p>${dinner.itemDescription}</p>
+              <span>R:${dinner.priceCents}</span>
+            </div>
+          </div>`
+        
+      });
+      break;
+
+    default:
+      
+
+      return;
+  }
+//pushing the menus to html
+  menuItemsContainer.innerHTML = menuItemsHTML;
+
+  // Remove active class from links
+  const menuLinks = document.querySelectorAll('.menu-row-one ul li a');
+  menuLinks.forEach(link => link.classList.remove('active'));
+
+  // Add active class to the clicked link
+  const activeLink = document.querySelector(`.menu-row-one ul li a[href="#"][onclick="showMenu('${category}']"]`);
+  if (activeLink) {
+    activeLink.classList.add('active');
+  }
+}
+
