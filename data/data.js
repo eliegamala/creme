86b7a1043e73e96ddb0fca599e840f49a2916c9d
eliegamala <eliegamala6@gmail.com>
2024-07-14 09:40:20 +0200

    create a separate folder file to store the data , which will make it easier read

---
+++ data/data.js
@@ -0,0 +1,159 @@
+//data for the blog articles
+const blogArticles = [
+  {
+    image: 'images/popularitem1.jpg',
+    title: 'delicious',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/article1.html'
+  },
+  {
+    image: 'images/popularitem2.jpg',
+    title: 'orange cake',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/vanillaCake.html'
+  },
+  {
+    image: 'images/popularitem4.jpg',
+    title: 'chocolate icecream',
+    description: 'lorem ipsum doreat do reme fas si',
+    articleLink: '../blog/chocolateicecrem.html'
+  }
+];
+
+//data for the latest blog articles
+const latestBlogArticles = [
+  {
+    image: 'images/popularitem1.jpg',
+    title: 'Delicious',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/article1.html'
+  },
+  {
+    image: 'images/popularitem2.jpg',
+    title: 'Orange Cake',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/vanillaCake.html'
+  },
+  {
+    image: 'images/popularitem4.jpg',
+    title: 'Chocolate Ice Cream',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    articleLink: '../blog/chocolateicecrem.html'
+  }
+];
+
+
+
+// Special menu products data
+const specialProducts = [
+  {
+    image: 'images/specialfood1.jpg',
+    itemName: 'cherry cake',
+    itemDescription: 'lorem ipsun dokar sit',
+    priceCents: 43
+  },
+  {
+    image: 'images/specialfood2.jpg',
+    itemName: 'strawberry cake',
+    itemDescription: 'heaven on eart ipsum cherry',
+    priceCents: 54
+  },
+  {
+    image: 'images/speicalfood3.jpg',
+    itemName: 'vanilla cake',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 100
+  },
+  {
+    image: 'images/specialfood4.jpg',
+    itemName: 'strawberry cupcake',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 32
+  }
+];
+
+
+
+// Breakfast menu products
+const breakfastProducts = [
+  {
+    image: 'images/breakfast1.jpg',
+    itemName: 'croissant and cafe',
+    itemDescription: 'lorem ipsun dokar sit',
+    priceCents: 43
+  },
+  {
+    image: 'images/breakfast2.jpg',
+    itemName: '2 coffees special',
+    itemDescription: 'heaven on eart ipsum cherry',
+    priceCents: 54
+  },
+  {
+    image: 'images/breakfast3.jpg',
+    itemName: 'cookie and coffee',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 100
+  },
+  {
+    image: 'images/breakfast4.jpg',
+    itemName: 'slice cake and coffee',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 32
+  }
+];
+
+// Dinner menu products
+const dinnerProducts = [
+  {
+    image: 'images/dinner1.jpg',
+    itemName: 'lemon cake',
+    itemDescription: 'lorem ipsun dokar sit',
+    priceCents: 43
+  },
+  {
+    image: 'images/dinner2.jpg',
+    itemName: 'circle cake',
+    itemDescription: 'heaven on eart ipsum cherry',
+    priceCents: 54
+  },
+  {
+    image: 'images/dinner3.jpg',
+    itemName: 'strawberry pie cake',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 100
+  },
+  {
+    image: 'images/dinner4.jpg',
+    itemName: 'croissant',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 32
+  }
+];
+
+// Lunch menu products
+const lunchProducts = [
+  {
+    image: 'images/lunch1.jpg',
+    itemName: 'waffle and cream',
+    itemDescription: 'lorem ipsun dokar sit',
+    priceCents: 43
+  },
+  {
+    image: 'images/lunch2.jpg',
+    itemName: 'waffle and fruit',
+    itemDescription: 'heaven on eart ipsum cherry',
+    priceCents: 54
+  },
+  {
+    image: 'images/lunch3.jpg',
+    itemName: 'pancake with strawberry',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 100
+  },
+  {
+    image: 'images/lunch4.jpg',
+    itemName: 'bread and berries',
+    itemDescription: 'lorem ipsun dokar sit ipsimslo',
+    priceCents: 32
+  }
+];
