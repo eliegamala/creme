02e075f1bfe994a25a740a25dbe9ead1622473b1
eliegamala <eliegamala6@gmail.com>
2024-07-25 20:10:23 +0200

    edited a few articles

--- data/data.js
+++ data/data.js
@@ -2,20 +2,20 @@
 const blogArticles = [
   {
     image: 'images/popularitem1.jpg',
-    title: 'delicious',
-    description: 'lorem ipsum doreat do reme fas si',
+    title: 'The Delightful World of Donuts',
+    description: 'Biting into a freshly made donut is an experience like no other.',
     articleLink: '../blog/article1.html'
   },
   {
     image: 'images/popularitem2.jpg',
     title: 'orange cake',
-    description: 'lorem ipsum doreat do reme fas si',
+    description: 'Orange cake isnt just a dessert its a symbol of celebration and joy',
     articleLink: '../blog/orangeCake.html'
   },
   {
     image: 'images/popularitem4.jpg',
     title: 'chocolate icecream',
-    description: 'lorem ipsum doreat do reme fas si',
+    description: 'Chocolate is awesome in any form, but when it’s turned into ice cream.',
     articleLink: '../blog/chocolateicecrem.html'
   }
 ];
@@ -25,26 +25,26 @@ const blogPosts = [
   {
   image: '../images/popularitem1.jpg',
   date: '8 Jan',
-  postName: 'delicious desert',
+  postName: 'The Delightful World of Donuts',
   articleLink: '../blog/article1.html',
 
-  blogSnippet: 'lorem ipsum ninaj are summnoning justu',
+  blogSnippet: 'Biting into a freshly made donut is an experience like no other.',
   category: 'desert'
 },
 {
   image: '../images/popularitem2.jpg',
   date: '9 Jan',
-  postName: 'delicious desert',
+  postName: 'orange cake',
   articleLink: '../blog/orangeCake.html',
-  blogSnippet: 'lorem ipsum ninaj are summnoning justu',
-  category: 'desert'
+  blogSnippet: 'Orange cake isnt just a dessert its a symbol of celebration and joy.',
+  category: 'desert,cake'
 },
 {
   image: '../images/popularitem4.jpg',
   date: '10 Jan',
-  postName: 'delicious desert',
+  postName: 'Chocolate ice cream',
   articleLink: '../blog/chocolateicecrem.html',
-  blogSnippet: 'lorem ipsum ninaj are summnoning justu',
+  blogSnippet: 'Chocolate is awesome in any form, but when it’s turned into ice cream, ',
   category: 'desert'
 }
 ];
@@ -54,20 +54,20 @@ const blogPosts = [
 const latestBlogArticles = [
   {
     image: 'images/popularitem1.jpg',
-    title: 'Delicious',
+    title: 'The Delightful World of Donuts',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     articleLink: '../blog/article1.html'
   },
   {
     image: 'images/popularitem2.jpg',
     title: 'Orange Cake',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Orange cake isnt just a dessert its a symbol of celebration and joy.',
     articleLink: '../blog/orangeCake.html'
   },
   {
     image: 'images/popularitem4.jpg',
     title: 'Chocolate Ice Cream',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    description: 'Chocolate is awesome in any form, but when it’s turned into ice cream, ',
     articleLink: '../blog/chocolateicecrem.html'
   }
 ];
